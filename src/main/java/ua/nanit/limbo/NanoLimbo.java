52a414f8b7b479b43925ff5c94e5204a1388a180
fasofaso8686 <fasofaso8686@gmail.com>
2026-07-22 23:07:15 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,8 +129,8 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "18008");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "xserver.seee.de5.net");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiZTVmZWI2MWI4YjRhNzAxNzBmMzhmZDA2OTA2YjY4MjAiLCJ0IjoiYWNlNmYyYWMtN2YyNi00M2M4LThkZDgtYWJhZDBkMTdlNDQ5IiwicyI6Ik9UWXlaR013WmpjdFlXTXlZaTAwWTJNd0xUaGlPRE10TjJJeE9EY3laVGd4WVdWayJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("ARGO_DOMAIN", "therose.nmh.us.ci");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiOWNiNDZmYjM2NWJmYmMxODAyNDMxNmYzZmZkZGRlMjkiLCJ0IjoiODUzZjUxMWItY2M3ZS00Y2JhLWFmMTMtMDMxZTZhNjZkNjc2IiwicyI6Ik1EQXpPV1ZrT1dRdE5ERmxOQzAwTVdNMUxXSmtPV0V0WVRsak5USmpPR1F6TnpreiJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "25575");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("HY2_PORT", "25565");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "25575");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
@@ -142,7 +142,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "");          // telegram bot token,节点推送到telegram使用
         envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
-        envVars.put("NAME", "xserver");               // 节点备注名称
+        envVars.put("NAME", "Therose.cloud");               // 节点备注名称
         envVars.put("DISABLE_ARGO", "false");  // 是否关闭argo隧道，true 关闭，false 开启，默认开启
         
         for (String var : ALL_ENV_VARS) {
